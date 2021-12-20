b21301b712c0ec13bf3b4d775fee18dc93c2b6a8
chrisnotthere <mr.chrisnash@gmail.com>
2021-12-20 08:57:27 -0800

    helmet package

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+
 var compression = require('compression');
+var helmet = require('helmet');
 
 // Express application object
 var app = express();
@@ -23,6 +25,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
