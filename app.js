e962f7a8ea886bd4d84a509c281a8e40b87fb829
chrisnotthere <mr.chrisnash@gmail.com>
2021-12-20 11:58:14 -0800

    prepared for heroku deployment

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://libAdmin:admin123@cluster0.xfjqq.mongodb.net/library-tut?retryWrites=true&w=majority';
+//var mongoDB = 'mongodb+srv://libAdmin:admin123@cluster0.xfjqq.mongodb.net/library-tut?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://libAdmin:admin123@cluster0.xfjqq.mongodb.net/library-tut?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
